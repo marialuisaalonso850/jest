c3ab0039e02cec15e37e3c486cf27a67fe2a33fe
Maria Luisa <117184776+marialuisaalonso850@users.noreply.github.com>
2023-11-15 05:40:30 -0500

    Update index.spec.js
    cambio

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,10 +1,48 @@
 import app from "../src/app.js"
 import request from "supertest"
 
-describe("GET/tasks",()=>{
+describe("GET /tasks", () => {
+  test("should respond with a 200 status code", async () => {
+ const response = await request(app).get("/tasks").send();
+    expect(response.statusCode).toBe(200);
+  });
 
-    test("should respond with a 200 status code", async() => { 
-        const response = await request(app).get("/tasks").send();
-   expect(response.statusCode).toBe(200)
-    })
-})
+  test("should respond an array", async () => {
+    const response = await request(app).get("/tasks").send();
+    expect(response.body).toBeInstanceOf(Array);
+  });
+});
+
+describe("POST /tasks", () => {
+  describe("given a title and description", () => {
+    const newTask = {
+      title: "some title",
+      description: "some description",
+    };
+
+    
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/tasks").send(newTask);
+      expect(response.statusCode).toBe(200);
+    });
+    
+    test("should respond with an task ID", async () => {
+      const response = await request(app).post("/tasks").send(newTask);
+      expect(response.body.id).toBeDefined();
+    });
+  });
+
+  describe("when the title and description is missing", () => {
+    test("shoud respond with a 400 status code", async () => {
+      const fields = [
+        { title: "some title" },
+        { description: "some description" },
+      ];
+
+      for (const body of fields) {
+        const response = await request(app).post("/tasks").send(body);
+        expect(response.statusCode).toBe(400);
+      }
+    });
+  });
+});
